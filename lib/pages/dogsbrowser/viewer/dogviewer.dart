fb10f76ea5a45df8aa6a868a217ee447444f1b17
Lewis Cianci <lewis@MacBook-Pro.local>
2021-05-08 10:05:43 +1000

    initial routing changes

--- lib/pages/dogsbrowser/viewer/dogviewer.dart
+++ lib/pages/dogsbrowser/viewer/dogviewer.dart
@@ -1,5 +1,7 @@
 import 'dart:typed_data';
+import 'dart:ui';
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -19,18 +21,61 @@ class DogViewerPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(name),
       ),
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            child: Hero(
-              tag: name,
-              child: Image.memory(
-                imageBytes,
-                fit: BoxFit.contain,
+          Hero(
+            tag: name,
+            child: Material(
+              child: Stack(
+                // alignment: Alignment.bottomCenter,
+                // fit: StackFit.expand,
+                children: [
+                  Image.memory(
+                    imageBytes,
+                    height: MediaQuery.of(context).size.height - 100,
+                    fit: BoxFit.fitHeight,
+                  ),
+                  SingleChildScrollView(
+                      // primary: ,
+                      // dragStartBehavior: DragStartBehavior.start,
+                      child: Column(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height - 100,
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(40),
+                        ),
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(sigmaY: 15, sigmaX: 15),
+                          child: Container(
+                            child: Center(
+                              child: Text(
+                                name,
+                                textAlign: TextAlign.center,
+                                style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                                      fontSize: 30,
+                                      color: Colors.white.withAlpha(200),
+                                    ),
+                              ),
+                            ),
+                            height: 500,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              color: Theme.of(context).colorScheme.primary.withAlpha(30),
+                              // backgroundBlendMode: BlendMode.luminosity,
+                            ),
+                            // height: 50,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ))
+                ],
               ),
             ),
-          ),
-          Text(name)
+          )
         ],
       ),
     );
